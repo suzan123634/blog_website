c414df5169fa0caef3d287ab98e2baf897d57cbe
Ashesh Shrestha <asheshshrestha2@gmail.com>
2020-09-10 20:50:49 +0545

    delete mirror file

--- blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from blog.models import Blog
-
-class CreateBlogForm(forms.ModelForm):
-    class Meta:
-        model = Blog
-        fields = "title", "highlight", "article", "author", "category", "image"
